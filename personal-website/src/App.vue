c7a066ae262d94ad00d8170252d5aaab3a0c1cc8
Patrick <pfh28@users.noreply.github.com>
2022-07-31 00:26:17 -0400

    test adding temp wedding site

---
+++ personal-website/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col items-center">
+    <div class="pb-4">
+      <img class="drop-shadow-lg rounded-lg h-4/5 max-h-[80vh]" :src="require('./assets/sq_portrait.JPG')" />
+    </div>
+    <div class="text-4xl font-script">Christine and Patrick</div>
+    <div class="text-md font-serif">will be married on</div>
+    <div class="text-3xl font-script">April <span class="text-2xl">22</span>, 2023</div>
+    <div class="text-md font-serif">Watch this space for additional details</div>
+  </div>
+</template>
+
+
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+
